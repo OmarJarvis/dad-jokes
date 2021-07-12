b7f7610ce35ce543ceb85e9424dcb6f047d79700
omar <thejarvisgroupllc@gmail.com>
2021-07-11 21:39:02 -0400

    Completed Assignments

--- index.js
+++ index.js
@@ -2,18 +2,35 @@ const button = document.querySelector(".container button");
 const jokeText = document.querySelector(".container p");
 document.addEventListener("DOMContentLoaded", getJoke);
 
+const likeBtn = document.querySelector(".like__btn");
+let likeIcon = document.querySelector("#icon"),
+  count = document.querySelector("#count");
+
+let clicked = false;  
+
 button.addEventListener("click", getJoke);
 
-async function getJoke() {
-    const jokeData = await fetch('https://icanhazdadjoke.com/', {
-        headers: {
+function getJoke(){
+    fetch('https://icanhazdadjoke.com/',{
+        headers:{
             'Accept': 'application/json'
         }
+    }).then(data=> data.json())
+      .then(obj => jokeText.innerHTML = obj.joke) 
+}
 
-    });
-    const jokeObj = await jokeData.json();
-    jokeText.innerHTML = jokeObj.joke;
-} 
+likeBtn.addEventListener("click", () => {
+  if (!clicked) {
+    clicked = true;
+    likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+    count.textContent++;
+  } else {
+    clicked = false;
+    likeIcon.innerHTML = `<i class="far fa-thumbs-up">
+    </i>`;
+    count.textContent--;
+  }
+});
      
     
 
