b1e7fa3682a49909c22ff5c57cfa30cb5ea2f183
omar <thejarvisgroupllc@gmail.com>
2021-07-16 09:59:22 -0400

    Completed assignments

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const audio = document.getElementById("myAudio");
 const like = document.getElementById('like') 
 const count = document.getElementById('counter');
 like.addEventListener('click', myfunction)
-
+   
+    
 function myfunction() {
       count.innerHTML = parseInt(count.innerHTML) +1;
     }
@@ -19,11 +20,17 @@ async function getJoke() {
     });
     const jokeObj = await jokeData.json();
     jokeText.innerHTML = jokeObj.joke;
+    
 } 
 
 function playAudio() { 
   audio.play(); 
 } 
+
+
+
+
+
 
 
     
